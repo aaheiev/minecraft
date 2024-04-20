e8a297aaacdb043632352c7195ead3b16bf81eb0
Andrii Aheiev <a.ageyev@gmail.com>
2024-04-20 20:48:54 +0200

    iac image workflow

--- bootstrap/backend_storage.tf
+++ bootstrap/backend_storage.tf
@@ -1,8 +1,9 @@
 resource "google_storage_bucket" "terraform_backend" {
-  name     = var.tf_backend_google_storage_bucket_name
-  project  = var.gcp_project
-  location = var.tf_backend_google_storage_bucket_location
-  labels   = {}
+  name                        = var.tf_backend_google_storage_bucket_name
+  project                     = var.gcp_project
+  location                    = var.tf_backend_google_storage_bucket_location
+  uniform_bucket_level_access = true
+  labels                      = {}
 }
 
 resource "google_storage_bucket_iam_member" "bucket_admin" {
